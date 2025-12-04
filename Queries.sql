0ff638a33a0a4d4cfdd13625d7d0ac2c2e53850e
Abobus_bedesher <98686283+qwerty123456gd@users.noreply.github.com>
2025-12-04 15:58:04 +0600

    Update and rename Запросы.sql to Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -33,3 +33,4 @@ and klients.last_name = 'Павлов'
 and DATE_PART('month', used_s.udate) = DATE_PART('month', CURRENT_DATE) 
 and tar_in_tel.tel_id = number_of_telephone.id and tar_in_tel.tar_id = Tarifs.id and addservices.id = used_s.s_id);*/
 
+
